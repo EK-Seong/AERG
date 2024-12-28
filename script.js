fd7f70d457a02a4141a1f5826808f1f3ab4417e5
Eunkyu Seong <131163793+EK-Seong@users.noreply.github.com>
2024-12-28 23:11:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop - 50,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
